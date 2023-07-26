b764c9cb02b031896791e5be0071dbe14401b6c8
Aditi Dadariya <103076396+aditidadariya@users.noreply.github.com>
2023-07-26 23:53:44 +0100

    Uploaded request.py file
    Uploaded request.py file

---
+++ request.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 13:40:03 2023
+
+@author: aditidadariya
+"""
+
+import requests
+url = 'http://localhost:5000/predict_api'
+
+r = requests.post(url,json={"AGE":153,
+                            "JOB":0,
+                            "MARITAL":1,
+                            "EDUCATION":0,
+                            "DEFAULT":12,
+                            "HOUSING":7,
+                            "LOAN":1.25,
+                            "CONTACT":1,
+                            "MONTH":1,
+                            "DAY_OF_WEEK":1,
+                            "DURATION":0,
+                            "CAMPAIGN":0,
+                            "PDAYS":68,
+                            "PREVIOUS":0,
+                            "POUTCOME":0,
+                            "EMP.VAR.RATE":0,
+                            "CONS.PRICE.IDX":0,
+                            "CONS.CONF.IDX":0,
+                            "EURIBOR3M": 0,
+                            "NR.EMPLOYED":0
+                            })
+
+print(r.json())
+
